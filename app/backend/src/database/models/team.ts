3b8b56b3cfa89a757e8dc852f58ced7b5f02fa79
Amanda Perch <perch_amanda@hotmail.com>
2022-09-29 18:40:20 -0300

    Model team

--- app/backend/src/database/models/team.ts
+++ app/backend/src/database/models/team.ts
@@ -1,30 +1,27 @@
-import { Model } from 'sequelize';
+import { INTEGER, Model, STRING } from 'sequelize';
 import db from '.';
-// import OtherModel from './OtherModel';
 
-class Example extends Model {
-  // public <campo>!: <tipo>;
+class Team extends Model {
+  id!: number;
+  teamName!: string;
 }
 
-Example.init({
-  // ... Campos
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
 }, {
-  // ... Outras configs
-  underscored: true,
   sequelize: db,
-  // modelName: 'example',
+  modelName: 'teams',
   timestamps: false,
 });
 
-/**
-  * `Workaround` para aplicar as associations em TS: 
-  * Associations 1:N devem ficar em uma das instâncias de modelo
-  * */
-
-// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
-// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-
-// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
-
-export default Example;
+export default Team;
