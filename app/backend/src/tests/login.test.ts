15bcf5dceb70ec05f075536648dd7d3478a1069d
Amanda Perch <perch_amanda@hotmail.com>
2022-09-30 17:03:10 -0300

    Escrevendo describes e its para teste

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,101 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/user';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { request, expect } = chai;
+
+//describe('', () => { it('',() => {}) }) - Exemplo de inicio de teste para usar em outros arquivos
+
+describe('User and Login', () => {
+  // questão 02
+  describe('Acesso com dados válidos no Front-end' , () => {
+    it('A rota utilizada é do tipo POST',() => {
+
+    })
+
+    it('É possivel fazer login com os dados informados',() => {
+
+    })
+
+    it('A senha que existe no bando estão encriptadas',() => {
+
+    })
+
+    it('Está retornando o status 200',() => {
+
+    })
+  })
+  // questão 04
+  describe('Não terá o acesso sem informar e-mail', () => {
+    it('Login sem e-mail tem o retorno de bad request', () => {
+
+    })
+    it('Não tem o campo e-mail o retorno 400 , com a mensagem "All Fields must be filled" ', () => {
+
+    })
+  })
+  // questão 06 
+  describe('Não terá o acesso sem informar senha', () => {
+    it('Login sem senha tem o retorno de bad request', () => {
+
+    })
+    it('Não tem o campo "password" o retorno 400 , com a mensagem "All Fields must be filled" ', () => {
+
+    })
+  })
+  // questão 08 
+  describe('Não terá acesso com um email inválido', () => { 
+    it('Login sem  email incorreto retornará status não-autorizado', () => {
+
+    })
+    it('Campo  "email" inválido o retorno 400 , com a mensagem "Incorrect email or password" ', () => {
+
+    })
+  })
+  // questão 10 
+  describe('Não terá acesso com um senha inválida', () => {
+    it('Login sem  senha incorreto retornará status não-autorizado', () => {
+
+    })
+    it('Campo  "senha" inválido o retorno 400 , com a mensagem "Incorrect email or password" ', () => {
+
+    })
+  })
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
+
+  // let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Example, "findOne")
+  //     .resolves({
+  //       ...<Seu mock>
+  //     } as Example);
+  // });
+
+  // after(()=>{
+  //   (Example.findOne as sinon.SinonStub).restore();
+  // })
+
+  // it('...', async () => {
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      ...
+
+  //   expect(...)
+  // });
+
+  // it('Seu sub-teste', () => {
+  //   expect(false).to.be.eq(true);
+  // });
+});
