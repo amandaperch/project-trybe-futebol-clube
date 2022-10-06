86e559ad5de114751ca55b8c3d24a3a07dbcfb3b
Amanda Perch <perch_amanda@hotmail.com>
2022-10-06 17:34:59 -0300

    Matches com erro no false

--- app/backend/src/Controllers/matches.controller.ts
+++ app/backend/src/Controllers/matches.controller.ts
@@ -3,7 +3,18 @@ import MatchesService from '../Service/matches.service';
 
 export default class MatchController {
   static async getAll(req: Request, res: Response) {
-    const Match = await MatchesService.getAll();
-    return res.status(200).json(Match);
+    const { inProgress } = req.query;
+    if (!inProgress) {
+      const allMatches = await MatchesService.getAll();
+      return res.status(200).json(allMatches);
+    }
+    if (inProgress === 'true') {
+      const matchesInProgress = await MatchesService.getByProgress(true);
+      return res.status(200).json(matchesInProgress);
+    }
+    if (inProgress === 'false') {
+      const matchesFinished = await MatchesService.getByProgress(false);
+      return res.status(200).json(matchesFinished);
+    }
   }
 }
--- app/backend/src/Service/matches.service.ts
+++ app/backend/src/Service/matches.service.ts
@@ -1,8 +1,23 @@
 import Matches from '../database/models/match';
+import Team from '../database/models/team';
 
 export default class MatchesService {
   static getAll = async () => {
-    const allTeams = await Matches.findAll();
+    const allTeams = await Matches.findAll({
+      include: [
+        { model: Team, as: 'teamHome', attributes: ['teamName'] },
+        { model: Team, as: 'teamAway', attributes: ['teamName'] },
+      ] });
+    return allTeams;
+  };
+
+  static getByProgress = async (inProgress: boolean) => {
+    const allTeams = await Matches.findAll({
+      where: { inProgress },
+      include: [
+        { model: Team, as: 'teamHome', attributes: ['teamName'] },
+        { model: Team, as: 'teamAway', attributes: ['teamName'] },
+      ] });
     return allTeams;
   };
 }
